270732554330c93b7fb1306735c9d93e67404165
Joscha Burkholz <joscha.burkholz@codecentric.de>
2017-11-21 08:57:56 +0100

    add sources

---
+++ src/main/java/de/joscha_burkholz/MeetupHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package de.joscha_burkholz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetupHelloWorldApplication.class, args);
+	}
+}
---
+++ src/test/java/de/joscha_burkholz/MeetupHelloWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package de.joscha_burkholz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MeetupHelloWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
